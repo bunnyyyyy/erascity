a46554168ea39d457173184a6aeb7222036f1cab
bunnyyy <sanya.badhe@gmail.com>
2023-05-02 18:42:15 -0700

    added taylor

--- MyJFrame.java
+++ MyJFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyJFrame {
     public static void main(String[] args) {
@@ -7,7 +8,16 @@ public class MyJFrame {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel() {
+           
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Image image = new ImageIcon(getClass().getResource("taylorswifthead.jpg")).getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+
+        };
+
         frame.add(panel);
         
         frame.setVisible(true);
